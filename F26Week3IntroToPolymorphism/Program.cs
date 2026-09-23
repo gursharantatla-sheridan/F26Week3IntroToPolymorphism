7b78509c4fa103a39fa280068a5ea24df49a4230
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-23 14:35:44 -0400

    static polymorphism

--- F26Week3IntroToPolymorphism/Program.cs
+++ F26Week3IntroToPolymorphism/Program.cs
@@ -4,7 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x = 5;
+            Add(3, 4, out x);
+            Add(3, 4);
+            Add("hello", "world");
+
+            Add(4, 5);
+
+            Add(3, 4);
+
+            int[] a = { 3, 4, 5 };
+            PrintArray(a);
+            //PrintArray(2, 3, 4, 5, 6);
         }
+
+        static void PrintArray(int[] arr)
+        {
+            // method body
+        }
+
+        //static void PrintArray(params int[] arr)
+        //{
+        //    // method body
+        //}
+
+        static int Add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        //static void Add(int n1, int n2)
+        //{
+        //    // method body
+        //}
+
+        static string Add(string n1, string n2)
+        {
+            return n1 + n2;
+        }
+
+        static int Add(int n1, int n2, int n3)
+        {
+            return n1 + n2 + n3;
+        }
+
+        static void Add(int n1, int n2, out int n3)
+        {
+            n3 = n1 + n2;
+        }
+
+        
     }
 }
